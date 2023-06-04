b973d84dd7b812d45cde70ff83943a84bb6524f6
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-06-04 17:36:42 +0200

    add user profile button to main page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, forwardRef } from 'react';
 import { useSession, signOut } from "next-auth/react";
 import { getSession } from 'next-auth/react'
 
@@ -24,6 +24,23 @@ function NoteCard ( {note_information} ) {
 export default function Feed () {
     const { data: session } = useSession()
     const user_id = session?.user?.email
+
+    const [showProfileCard, setShowProfileCard] = useState(false);
+    const profileCardRef = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (profileCardRef.current && !profileCardRef.current.contains(event.target)) {
+            setShowProfileCard(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+    }, []);
 
     const [inputValue, setInputValue] = useState('');
 
@@ -69,6 +86,41 @@ export default function Feed () {
 
     const notes_list = list.map(note => <NoteCard key={note.uuid} note_information={note}/> )
 
+    function ProfilePicButton({onClick}) {
+        return (
+            <button
+                onClick={onClick}
+                style={{ position: "fixed", bottom: "30px", left: "30px", padding: "0", borderRadius: "50%", width: "50px", height: "50px", boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)" }}>
+                <img src="/profile_pic.png" alt="A profile picture" style={{ borderRadius: "50%", width: "50px", height: "50px" }} />
+            </button>
+        );
+    }
+
+    const ProfileCard = forwardRef((props, ref) => {
+        return (
+            <div
+                ref={ref}
+                style={{
+                    position: "fixed",
+                    bottom: "70px",
+                    left: "30px",
+                    backgroundColor: "#fff",
+                    padding: "10px",
+                    borderRadius: "15px",
+                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}
+            >
+                <img src="/profile_pic.png" alt="A profile picture" style={{ borderRadius: "50%", width: "50px", height: "50px" }} />
+                <p>{user_id}</p>
+                <button style={{ width: "100%", margin: "5px 0" }}>Settings</button>
+                <button style={{ width: "100%", margin: "5px 0" }} onClick={() => signOut()}>Log out</button>
+            </div>
+        );
+    });
+
     // Load notes on initial render
     useEffect(() => {
         fetch('/api/get-notes')
@@ -88,10 +140,14 @@ export default function Feed () {
                 <form id="input" onSubmit={handleSubmit}>
                     <input type="text" value={inputValue} onChange={handleChange}/>
                     <button type="submit" id="post">Post Note</button>
-                    <button onClick={() => signOut()}>Sign out</button>
                 </form>
             </div>
             <div ref={endOfNotes} />
+            <ProfilePicButton onClick={() => setShowProfileCard(true)} />
+            {showProfileCard &&
+                <ProfileCard
+                    ref={profileCardRef}
+                />}
         </>
     )
 }
